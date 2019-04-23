e83542d5f0e205647465ed9c5274bbe912e9373e
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2019-04-23 17:32:17 +0200

    round tweak, now with #

--- src/components/roundCount.js
+++ src/components/roundCount.js
@@ -6,27 +6,27 @@ const RoundCount = (props) => (
     <div className="top">
 
         { (props.round === 1)
-            ? <p><span className="active">1</span>,2,3,4,5</p>
+            ? <p><span className="active">#1</span>,2,3,4,5</p>
             : null
         }
 
         { (props.round === 2)
-            ? <p>1,<span className="active">2</span>,3,4,5</p>
+            ? <p>1,<span className="active">#2</span>,3,4,5</p>
             : null
         }
 
         { (props.round === 3)
-            ? <p>1,2,<span className="active">3</span>,4,5</p>
+            ? <p>1,2,<span className="active">#3</span>,4,5</p>
             : null
         }
 
         { (props.round === 4)
-            ? <p>1,2,3,<span className="active">4</span>,5</p>
+            ? <p>1,2,3,<span className="active">#4</span>,5</p>
             : null
         }
 
         { (props.round === 5)
-            ? <p>1,2,3,4,<span className="active">5</span></p>
+            ? <p>1,2,3,4,<span className="active">#5</span></p>
             : null
         }
         
