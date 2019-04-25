f7e25304eb71b53198bd57680d402a704875668f
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2019-04-25 21:49:56 +0200

    get the emoji icons outta here

--- src/components/menu.js
+++ src/components/menu.js
@@ -14,14 +14,11 @@ class Menu extends Component {
     render () {
         return (
             <div className="menu">
-                <h1><span role="img" aria-label="age guiz">👵</span></h1>
-                <button type="button" onClick={() => {this.props.play()}}>start</button>
+                <img src="/face.svg" alt="actor" />
                 <p>Swipe and press the numbers to guess the actor's age when their movie premiered.</p>
-                <br/>
-                <p>5 points per correct guess, -1 point per year off.</p>
-                <br />
-                <p>Made for handheld devices with React & Sanity.io, by Ole Petter Baugerød Stokke.</p>
-
+                <br/>                
+                <p>Made for handheld devices with React & Sanity.io by Ole Petter Baugerød Stokke.</p>
+                <button type="button" onClick={() => {this.props.play()}}>start</button>
             </div>
 
         )
--- src/components/question.js
+++ src/components/question.js
@@ -3,9 +3,9 @@ import React from 'react';
 // render questions with icons
 const Question = props => (
     <div className="main">
-        <p>{props.question.actor} </p><img src="/face.svg" alt="" /><br/>
-        <p>{props.question.movie} </p><img src="/movie.svg" alt=""/><br/>
-        <p>{props.question.year} </p><img src="/eye.svg" alt=""/><br/>
+        <p>{props.question.actor} </p><img src="/face.svg" alt="actor" /><br/>
+        <p>{props.question.movie} </p><img src="/movie.svg" alt="movie"/><br/>
+        <p>{props.question.year} </p><img src="/eye.svg" alt="realesed"/><br/>
     </div>
 );
 
